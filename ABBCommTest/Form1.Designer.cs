d04a6b0d217fa51cd30a116f6e936bb3a1ed24d9
SDEBEUL <SDEBEUL@volvocars.com>
2017-06-22 12:41:01 +0200

    +added abb testproject for CFG deployment

---
+++ ABBCommTest/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ABBCommTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ControllerIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_addCntrl = new System.Windows.Forms.Button();
+            this.tbox_ip = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_getConf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ControllerIP});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(688, 249);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // ControllerIP
+            // 
+            this.ControllerIP.Tag = "ControllerIP";
+            this.ControllerIP.Width = 300;
+            // 
+            // btn_addCntrl
+            // 
+            this.btn_addCntrl.Location = new System.Drawing.Point(13, 301);
+            this.btn_addCntrl.Name = "btn_addCntrl";
+            this.btn_addCntrl.Size = new System.Drawing.Size(108, 23);
+            this.btn_addCntrl.TabIndex = 1;
+            this.btn_addCntrl.Text = "btn_addCntrl";
+            this.btn_addCntrl.UseVisualStyleBackColor = true;
+            this.btn_addCntrl.Click += new System.EventHandler(this.btn_addCtrl_Click);
+            // 
+            // tbox_ip
+            // 
+            this.tbox_ip.Location = new System.Drawing.Point(150, 301);
+            this.tbox_ip.Name = "tbox_ip";
+            this.tbox_ip.Size = new System.Drawing.Size(151, 22);
+            this.tbox_ip.TabIndex = 2;
+            this.tbox_ip.Text = "10.205.94.240";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(13, 379);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "btn_start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_startTask_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(13, 409);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "btn_stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_startStop_Click);
+            // 
+            // btn_getConf
+            // 
+            this.btn_getConf.Location = new System.Drawing.Point(13, 439);
+            this.btn_getConf.Name = "btn_getConf";
+            this.btn_getConf.Size = new System.Drawing.Size(75, 23);
+            this.btn_getConf.TabIndex = 5;
+            this.btn_getConf.Text = "btn_getConf";
+            this.btn_getConf.UseVisualStyleBackColor = true;
+            this.btn_getConf.Click += new System.EventHandler(this.btn_getConf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 488);
+            this.Controls.Add(this.btn_getConf);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.tbox_ip);
+            this.Controls.Add(this.btn_addCntrl);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btn_addCntrl;
+        private System.Windows.Forms.TextBox tbox_ip;
+        private System.Windows.Forms.ColumnHeader ControllerIP;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_getConf;
+    }
+}
+
---
+++ ABBCommTest/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.RapidDomain;
+using ABB.Robotics.Controllers.EventLogDomain;
+using ABB.Robotics.Controllers.ConfigurationDomain;
+
+
+namespace ABBCommTest
+{
+    public partial class Form1 : Form
+    {
+
+        private NetworkScanner scanner = null;
+        private Controller controller = null;
+        private ABB.Robotics.Controllers.RapidDomain.Task[] tasks = null;
+        private NetworkWatcher networkwatcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //scan and populate local subnet
+            this.scanner = new NetworkScanner();
+            this.scanner.Scan();
+            ControllerInfoCollection controllers = scanner.Controllers;
+            //populate the listview
+            ListViewItem item = null;
+            foreach (ControllerInfo controllerInfo in controllers)
+            {
+                item = new ListViewItem(controllerInfo.IPAddress.ToString());
+                item.SubItems.Add(controllerInfo.Id);
+                item.SubItems.Add(controllerInfo.Availability.ToString());
+                item.SubItems.Add(controllerInfo.IsVirtual.ToString());
+                item.SubItems.Add(controllerInfo.SystemName);
+                item.SubItems.Add(controllerInfo.Version.ToString());
+                item.SubItems.Add(controllerInfo.ControllerName);
+                this.listView1.Items.Add(item);
+                item.Tag = controllerInfo;
+            }
+
+            //monitor the network
+            this.networkwatcher = new NetworkWatcher(scanner.Controllers);
+            this.networkwatcher.Found += new EventHandler<NetworkWatcherEventArgs>(HandleFoundEvent);
+           // this.networkwatcher.Lost += new EventHandler<NetworkWatcherEventArgs>(HandleLostEvent);
+            this.networkwatcher.EnableRaisingEvents = true;
+
+            //enable monitoring for each controller 
+            foreach (Controller ctrl in controllers)
+            {
+                ctrl.OperatingModeChanged += new EventHandler<OperatingModeChangeEventArgs>(ctrl_OperatingModeChanged);
+            }
+
+
+        }
+
+        //adding a found controller to the listvieuw
+        private void AddControllerToListView(object sender, NetworkWatcherEventArgs e)
+        {
+            ControllerInfo controllerInfo = e.Controller;
+            ListViewItem item = new ListViewItem(controllerInfo.IPAddress.ToString());
+            item.SubItems.Add(controllerInfo.Id);
+            item.SubItems.Add(controllerInfo.Availability.ToString());
+            item.SubItems.Add(controllerInfo.IsVirtual.ToString());
+            item.SubItems.Add(controllerInfo.SystemName);
+            item.SubItems.Add(controllerInfo.Version.ToString());
+            item.SubItems.Add(controllerInfo.ControllerName);
+            this.listView1.Items.Add(item); 
+            item.Tag = controllerInfo;
+        }
+
+        private void SignalControlerLostToListView(object sender, NetworkWatcherEventArgs e)
+        {
+            //set listview item for controler Gray 
+        }
+
+        //handle event from networkscanner (controler found)
+        void HandleFoundEvent(object sender, NetworkWatcherEventArgs e)
+        {
+            this.Invoke(new EventHandler<NetworkWatcherEventArgs>(AddControllerToListView), new Object[] { this, e });
+        }
+
+
+        //handle event from networkscanner (controler lost)
+        void HandleLostEvent(object sender, NetworkWatcherEventArgs e)
+        {
+            this.Invoke(new EventHandler<NetworkWatcherEventArgs>(AddControllerToListView), new Object[] { this, e });
+        }
+
+
+        //event handler for op mode change
+        private static void ctrl_OperatingModeChanged(object sender, OperatingModeChangeEventArgs e)
+        {
+            Console.WriteLine("New Operating mode at: {0} new mode is: {1}", e.Time, e.NewMode);
+            MessageBox.Show(string.Format("New Operating mode at: {0} new mode is: {1}", e.Time, e.NewMode));
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem item = this.listView1.SelectedItems[0];
+
+            if (item.Tag != null)
+            {
+                ControllerInfo controllerInfo = (ControllerInfo)item.Tag;
+                if
+                (controllerInfo.Availability == Availability.Available)
+                {
+                    if (this.controller != null)
+                    {
+                        this.controller.Logoff();
+                        this.controller.Dispose();
+                        this.controller = null;
+                    }
+                    this.controller = ControllerFactory.CreateFrom(controllerInfo);
+                    this.controller.Logon(UserInfo.DefaultUser);
+                    if (controller.OperatingMode == ControllerOperatingMode.Auto)
+                    {
+                        tasks = controller.Rapid.GetTasks();
+                        using (Mastership m = Mastership.Request(controller.Rapid))
+                        {
+
+                           MessageBox.Show("Mastership");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Automatic mode is required to take master.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Selected controller not available.");
+                }
+            }
+        }
+
+        private void btn_startTask_Click(object sender, EventArgs e)
+        {
+            Ctrl_startStop(sender, e, true);
+        }
+
+        private void btn_startStop_Click(object sender, EventArgs e)
+        {
+            Ctrl_startStop(sender, e, false);
+        }
+
+        private void Ctrl_startStop(object sender, EventArgs e, Boolean TaskState)
+        {
+            try
+            {
+                if (controller.OperatingMode == ControllerOperatingMode.Auto)
+                {
+                    tasks = controller.Rapid.GetTasks();
+                    using (Mastership m = Mastership.Request(controller.Rapid))
+                    {
+                        //Perform operation
+                        if (TaskState)
+                        {
+                            tasks[1].Start();
+                            MessageBox.Show("Task started");
+                        }
+                        else
+                        {
+                            tasks[1].Stop();
+                            MessageBox.Show("Task Stopped");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Automatic mode is required to start execution from a remote client.");
+                }
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                MessageBox.Show("Mastership is held by another client." + ex.Message);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Unexpected error occurred: " + ex.Message);
+            }
+
+        }
+
+        // add a controler by ip 
+        private void btn_addCtrl_Click(object sender, EventArgs e)
+        {
+            //adding for specifc ip 
+            try
+            {
+                this.scanner = new NetworkScanner();
+                 NetworkScanner.AddRemoteController(tbox_ip.Text.ToString()); // need to handle non ip format
+                this.scanner.Scan();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btn_getConf_Click(object sender, EventArgs e)
+        {
+
+            ListViewItem item = this.listView1.SelectedItems[0];
+
+            if (item.Tag != null)
+            {
+                ControllerInfo controllerInfo = (ControllerInfo)item.Tag;
+                if
+                (controllerInfo.Availability == Availability.Available)
+                {
+                    if (this.controller != null)
+                    {
+                        this.controller.Logoff();
+                        this.controller.Dispose();
+                        this.controller = null;
+                    }
+                    this.controller = ControllerFactory.CreateFrom(controllerInfo);
+                    this.controller.Logon(UserInfo.DefaultUser);
+
+                    ConfigurationDatabase cfg = controller.Configuration;
+                    Domain sioDomain = controller.Configuration.SerialIO;
+                
+                    //test read parm
+                   // string[] path = { "SIO", "COM_APP","robot_ga","ServerAddress" };
+                   // string data = cfg.Read(path);
+                    //OK 
+
+                    //test write parm. (existing)
+                    /*
+                     try
+                        {
+
+                        using (Mastership m = Mastership.Request(controller.Configuration))
+                             {
+                                 string[] pathWRite = { "SIO", "COM_APP", "robot_ga", "LocalPath" };
+                                 cfg.Write("Test1:", pathWRite);
+                             } 
+                        }
+                     catch (System.InvalidOperationException ex)
+                        {
+                              MessageBox.Show("Mastership is held by another client.");
+                        }
+                     */
+                     // OK
+
+                     //test write parm. (new instance )
+                     try
+                     {
+                         DomainCollection domains = cfg.Domains;
+                         Domain domain = controller.Configuration.Domains[controller.Configuration.Domains.IndexOf("SIO")];
+                         ABB.Robotics.Controllers.ConfigurationDomain.Type COM_APPType = domain.Types[domain.Types.IndexOf("COM_APP")];
+
+                         using (Mastership m = Mastership.Request(controller.Configuration))
+                         {
+
+                             //COM_apptype should have al the instances of comm app type on the controller
+                             var objInstance = COM_APPType["Test_Instance"]; //looks if the instance we whant to create exist
+
+                             //create new instance of com_app
+                             if (objInstance == null) objInstance = COM_APPType.Create("Test_Instance"); //if not exist create it 
+
+                             // set an attribute
+                             objInstance.SetAttribute("LocalPath", "testpath:");
+                             
+
+                             //get it back 
+                             object objEntry = objInstance.GetAttribute("LocalPath");
+                  
+                         }
+                     }
+                     catch (System.InvalidOperationException ex)
+                     {
+                         MessageBox.Show("Mastership is held by another client.");
+                     }
+                    // 
+
+                }
+                else
+                {
+                    MessageBox.Show("Selected controller not available.");
+                }
+            }
+        }
+
+
+
+    }
+}
