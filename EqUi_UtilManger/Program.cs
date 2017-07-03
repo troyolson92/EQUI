fe1845892537f69463e8e9e850e0afacb175088d
SDEBEUL <SDEBEUL@volvocars.com>
2017-07-03 08:03:14 +0200

    +adding clickonce deployment to IIS with URL querystring

--- EqUi_UtilManger/Program.cs
+++ EqUi_UtilManger/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using NDesk.Options;
 using System.Deployment.Application;
+using System.Web;
+using System.Collections.Specialized;
 
 namespace EqUi_UtilManger
 {
@@ -15,100 +16,61 @@ namespace EqUi_UtilManger
         static void Main()
         {
             String[] Args = Environment.GetCommandLineArgs();
+            NameValueCollection col = new NameValueCollection();
             //
 
-            /*
             if (ApplicationDeployment.IsNetworkDeployed)
             {
                 string queryString = ApplicationDeployment.CurrentDeployment.ActivationUri.Query;
                 if (queryString != null)
                 {
                     MessageBox.Show("actURL= " + queryString);
+                    col = HttpUtility.ParseQueryString(queryString);
+                    HandleQueryParms(col);
                 }
                 {
-                    MessageBox.Show("actURL= " + "empty");
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
                 }
 
-            }*/
-
-
-            if (Args == null || Args.Length < 2 )      //direct run show launch form     
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
             }
-            else
-            {
-                bool show_help = false;
-                List<string> names = new List<string>();
-                int repeat = 1;
-
-                var p = new OptionSet() {
-                { "n|name=", "the {NAME} of someone to greet.",
-                   v => names.Add (v) },
-                { "r|repeat=", "the number of {TIMES} to repeat the greeting.\n this must be an integer.",
-                    (int v) => repeat = v },
-                { "v", "increase debug message verbosity",
-                   v => { if (v != null) Console.WriteLine("Verbosity"); } },
-                { "h|help",  "show this message and exit", 
-                   v => show_help = v != null },
-                };
 
 
-                List<string> extra;
-                try
-                {
-                    extra = p.Parse(Args);
-                }
-                catch (OptionException e)
-                {
-                    MessageBox.Show(string.Format(@"
-                    greet:
-                    {0}
-                    Try `greet --help' for more information.",
-                    e.Message));
-                    return;
-                }
-
-                if (show_help)
-                {
-                    ShowHelp(p);
-                    return;
-                }
-
-                string message;
-                if (extra.Count > 0)
-                {
-                    message = string.Join(" ", extra.ToArray());
-                    Debug("Using new message: {0}", message);
-                }
-                else
-                {
-                    message = "Hello {0}!";
-                    Debug("Using default message: {0}", message);
-                }
-
-                foreach (string name in names)
-                {
-                    for (int i = 0; i < repeat; ++i)
-                        Console.WriteLine(message, name);
-                }
-            }
         }
 
-        static void ShowHelp(OptionSet p)
+        private static void HandleQueryParms(NameValueCollection col)
         {
-            Console.WriteLine("Usage: greet [OPTIONS]+ message");
-            Console.WriteLine("Greet a list of individuals with an optional message.");
-            Console.WriteLine("If no message is specified, a generic greeting is used.");
-            Console.WriteLine();
-            Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(Console.Out);
-        }
+            foreach (string key in col.Keys)
+            {
+                if (string.IsNullOrEmpty(key)) continue;
 
-        static void Debug(string format, params object[] args)
-        {
+                string[] values = col.GetValues(key);
+                if (values == null) continue;
+
+                if (key == "Tool")
+                {
+                    switch (values[0])
+                    {
+                        case "SBCUstats":
+
+                            break;
+
+                        case "MaximoTool":
+
+                            break;
+
+                        default:
+                            MessageBox.Show(string.Format("Tool: '{0}' is not a valid tool",values[0]));
+                            break;
+
+
+                    }
+                }
+
+
+            }
+
         }
     }
 }
