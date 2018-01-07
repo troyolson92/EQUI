d0dc35aec6ad67dc06bfafe5b5d87869758a792f
SDEBEUL <SDEBEUL@volvocars.com>
2018-01-07 13:43:07 +0100

    popover basics working

--- EqUiWebUi/Areas/user_management/Controllers/userController.cs
+++ EqUiWebUi/Areas/user_management/Controllers/userController.cs
@@ -14,11 +14,11 @@ namespace EqUiWebUi.Areas.user_management.Controllers
     {
         private Models.userCookie db = new Models.userCookie();
 
-        // GET: user_management/user/Edit
-        public async Task<ActionResult> _Edit()
+        // GET: user_management/user/Details
+        public ActionResult _Details()
         {
             string username = System.Web.HttpContext.Current.User.Identity.Name;
-            users user = await db.L_users.Where(u => u.username == username).FirstAsync();
+            users user = db.L_users.Where(u => u.username == username).First();
             if (user == null)
             {
                 return HttpNotFound();
@@ -26,19 +26,31 @@ namespace EqUiWebUi.Areas.user_management.Controllers
             return PartialView(user);
         }
 
+        // GET: user_management/user/Edit
+        public async Task<ActionResult> Edit()
+        {
+            string username = System.Web.HttpContext.Current.User.Identity.Name;
+            users user = await db.L_users.Where(u => u.username == username).FirstAsync();
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
         // POST: user_management/user/Edit
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> _Edit([Bind(Include = "id,username,LocationRoot,AssetRoot,Locked,Blocked")] users user)
+        public async Task<ActionResult> Edit([Bind(Include = "id,username,LocationRoot,AssetRoot,Locked,Blocked")] users user)
         {
             if (ModelState.IsValid && !user.Locked )
             {
                 db.Entry(user).State = EntityState.Modified;
                 await db.SaveChangesAsync();
             }
-            return PartialView(user);
+            return View(user);
         }
 
     }
