7ab5b6cc5cb44df0096bb41edf84eff0ae570de5
SDEBEUL <SDEBEUL@volvocars.com>
2018-02-12 13:47:26 +0100

    fix bugged in alert system
    made changes to the way we style table rows. (A LOT CLEANER now)

---
+++ EqUiWebUi/App_Code/MyRowStyling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EqUiWebUi
+{
+    public static class MyRowStyling
+    {
+        public static string getRowStyleStatus(string status)
+        {
+            switch (status)
+            {
+                case "WGK":
+                    return "TableStatusWGK";
+
+                case "COMP":
+                    return "TableStatusCOMP";
+
+                default:
+                    return "";
+
+            }
+        }
+
+        public static string getRowStyleLogtype(string logtype)
+        {
+            switch (logtype)
+            {
+                case "SHIFTBOOK":
+                    return "TableLogtypeSHIFTBOOK";
+
+                case "WARNING":
+                    return "TableLogtypeWARNING";
+
+                case "LIVE":
+                    return "TableLogtypeLIVE";
+
+                case "BREAKDOWN":
+                    return "TableLogtypeBREAKDOWN";
+
+                case "TIMELINE":
+                    return "TableLogtypeTIMELINE";
+
+                default:
+                    return "";
+
+            }
+        }
+    }
+}
