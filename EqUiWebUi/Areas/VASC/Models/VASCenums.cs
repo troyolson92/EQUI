d78a33b49a0c1be4f9de541ee9369c623aec9fcc
De beul <SDEBEUL@volvocars.com>
2018-03-31 21:07:03 +0200

    more work on vasv ui

--- EqUiWebUi/Areas/VASC/Models/VASCenums.cs
+++ EqUiWebUi/Areas/VASC/Models/VASCenums.cs
@@ -24,6 +24,8 @@ namespace EqUiWebUi.Areas.VASC.Models
 
     public enum SQL_Action
     {
+        noAction = 0,
+
         Insert_a_new_record_into_the_rt_event_table = 0x01,
         Update_the_last_record_for_this_controller_event = 0x02,
         Use_the_event_for_breakdown_analysis = 0x04,
@@ -65,7 +67,7 @@ namespace EqUiWebUi.Areas.VASC.Models
 
     public enum Enable_bit_MASK
     {
-        Disabled = -1,
+        Disabled = 0,
 
         Enablebit1 = 0x01,
         Enablebit2 = 0x02,
@@ -93,4 +95,123 @@ namespace EqUiWebUi.Areas.VASC.Models
         obtained_PCSDK = 0x1,
         obtained_FTP = 0x2
     }
+
+    public enum Event_code
+    {
+        noEvent = 0, // ask GM if this is oke? 
+        //ABB
+        EVENT_CONTROLLER_OPERATING_MODE = 1,
+        EVENT_CONTROLLER_LOG = 2,
+        EVENT_SHUTDOWN = 3,
+        EVENT_LOG_MESSAGE = 4,
+        EVENT_CSV_FILE = 5,
+        EVENT_SIGNAL = 6,
+        EVENT_VARIABLE_SEARCH = 7,
+        EVENT_CYCLE_CHANGE = 8,
+        EVENT_RAPID_DATA_CHANGE = 8,
+        EVENT_PROGRAM_POSITION = 10,
+        EVENT_MOTION_POSITION = 11,
+        EVENT_RAPID_DATA_RESOLVE = 12,
+        EVENT_BACKUP = 13,
+        EVENT_MASTER = 14,
+        EVENT_TASK_ENABLED = 15,
+        EVENT_EXECUTION_CHANGE = 16,
+        EVENT_CONNECTION_CHANGE = 17,
+        EVENT_STATE_CHANGE = 18,
+        EVENT_CLEAR_LOGS = 19,
+        EVENT_POLL = 20,
+        EVENT_DEVICE_INFO = 21,
+        EVENT_TASK_EXECUTION_CHANGE = 22,
+        EVENT_DEAD = 23,
+        EVENT_PJV_ACTION = 24,
+        //Programmable codes
+        Cycle_start_code = 100,
+        Body_number = 101,
+        Application_error  = 102, //(0 in alarm, 1 not in alarm)
+        Cycle_end_code = 103,
+        //Register code
+        Register_1 = 200,
+        Register_2 = 201,
+        Register_3 = 202,
+        Register_4 = 203,
+        Register_5 = 204,
+        Register_6 = 205,
+        Register_7 = 206,
+        Register_8 = 207,
+
+    }
+
+    //From PCSDK for C_variable_search
+    public enum SymbolTypes
+    {
+        //
+        // Summary:
+        //     No type.
+        None = 0,
+        //
+        // Summary:
+        //     All atomic types.
+        Atomic = 1,
+        //
+        // Summary:
+        //     Records.
+        Record = 2,
+        //
+        // Summary:
+        //     Aliases.
+        Alias = 4,
+        //
+        // Summary:
+        //     Record components.
+        RecordComponent = 8,
+        //
+        // Summary:
+        //     Constants.
+        Constant = 16,
+        //
+        // Summary:
+        //     Variables.
+        Variable = 32,
+        //
+        // Summary:
+        //     Persistent data.
+        Persistent = 64,
+        //
+        // Summary:
+        //     Data, Constant | Variable | Persistent
+        Data = 112,
+        //
+        // Summary:
+        //     Parameters.
+        Parameter = 128,
+        //
+        // Summary:
+        //     Labels.
+        Label = 256,
+        //
+        // Summary:
+        //     Functions.
+        Function = 1024,
+        //
+        // Summary:
+        //     Procedures.
+        Procedure = 2048,
+        //
+        // Summary:
+        //     Traps.
+        Trap = 4096,
+        //
+        // Summary:
+        //     Routines, Function | Procedures | Trap.
+        Routine = 7168,
+        //
+        // Summary:
+        //     Modules.
+        Module = 8192,
+        //
+        // Summary:
+        //     Tasks.
+        Task = 16384
+    }
+
 }
