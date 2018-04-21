e907d03dc08dfe1010b550060a06355eb73afdb9
SDEBEUL <SDEBEUL@GNL1004ZCBQC2>
2018-04-21 15:06:15 +0200

    vasc breakdown details view
    fixxed table to view in entityframework

---
+++ EqUiWebUi/Areas/VASC/Controllers/BreakdownController.cs
@@ -0,0 +1,34 @@
+﻿using EqUiWebUi.Areas.VASC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.VASC.Controllers
+{
+    public class BreakdownController : Controller
+    {
+        private GADATAEntitiesVASC db = new GADATAEntitiesVASC();
+
+        // GET: VASC/Breakdown
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult rtJob(int jobId)
+        {
+            ViewBag.jobID = jobId;
+            return View();
+        }
+
+        public ActionResult _getJob(int jobID)
+        {
+            //need to add this in the bag
+            rt_job job = db.rt_job.Where(c => c.id == jobID).FirstOrDefault();
+            //return rt_job_breakdown list
+            return PartialView(db.rt_job_breakdown.Where(c => c.rt_job_active_id == jobID));
+        }
+    }
+}
---
+++ EqUiWebUi/Areas/VASC/Models/rt_job_breakdownMetadata.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EqUiWebUi.Areas.VASC.Models
+{
+    public class rt_job_breakdownMetadata
+    {
+    }
+
+    //c_service_setup metadata
+    [MetadataType(typeof(rt_job_breakdown))]
+    public partial class rt_job_breakdown
+    {
+        //this links our enum to the entitymodel
+        public Event_code _ev_breakdownAck
+        {
+            get
+            {
+                return (Event_code)Enum.ToObject(typeof(Event_code), this.ev_breakdownAck.GetValueOrDefault());
+            }
+            set
+            {
+                this.ev_breakdownAck = (int)value;
+            }
+        }
+
+        public Event_code _ev_breakdownStart
+        {
+            get
+            {
+                return (Event_code)Enum.ToObject(typeof(Event_code), this.ev_breakdownStart.GetValueOrDefault());
+            }
+            set
+            {
+                this.ev_breakdownStart = (int)value;
+            }
+        }
+
+
+    }
+}
