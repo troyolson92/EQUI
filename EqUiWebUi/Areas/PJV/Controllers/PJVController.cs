9fb22a30cc0cc86895fc8250472a3e83c35568fb
SDEBEUL <SDEBEUL@GNL1004ZCBQC2>
2018-05-01 17:16:13 +0200

    pjv basics

---
+++ EqUiWebUi/Areas/PJV/Controllers/PJVController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EqUiWebUi.Areas.PJV.Models;
+
+namespace EqUiWebUi.Areas.PJV.Controllers
+{
+    public class PJVController : Controller
+    {
+
+        private GADATAEntitiesPJV db = new GADATAEntitiesPJV();
+
+        // GET: PJV/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PJV/Modifications
+        public ActionResult GetChanges()
+        {
+            if (Session["LocationRoot"].ToString() != "")
+            {
+                string locationRoot = Session["LocationRoot"].ToString();
+                return View(db.ProcessPoints.Where(p => p.Delta > 0 && (p.locationtree ?? "").Contains(locationRoot)));
+            }
+            else
+            {
+                return View(db.ProcessPoints.Where(p => p.Delta > 0));
+            }
+
+        }
+
+        //GET: PJV/L_operation
+        public ActionResult GetL_operation()
+        {
+            return View(db.L_operation);
+        }
+
+    }
+}
---
+++ EqUiWebUi/Areas/VASC/Models/breakdownMetadata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EqUiWebUi.Areas.VASC.Models
+{
+    public class breakdownMetadata
+    {
+    }
+
+    //L_error metadata Exetension to get a cleaned up logtext
+    [MetadataType(typeof(L_error))]
+    public partial class L_error
+    {
+        //this is a short logtext representation
+        public string _Logtext
+        {
+            get
+            {
+                if (this.Title.Contains("ErrDisplay") || this.Title.Contains("External weld fault reported"))
+                {
+                    return this.L_description.Description.Trim();
+                }
+                else
+                {
+                    return this.Title.Trim();
+                }
+            }
+        }
+
+        //this is a full logtext representation
+        public string _FullLogtext
+        {
+            get
+            {
+                if (this.Title.Contains("ErrDisplay") || this.Title.Contains("External weld fault reported"))
+                {
+                    return this.L_description.Description.Trim();
+                }
+                else
+                {
+                    return this.Title.Trim() + Environment.NewLine + this.L_description.Description.Trim();
+                }
+            }
+        }
+
+
+    }
+}
