f0db465304e488cb9622de693e3315c2a383ee4e
SDEBEUL <SDEBEUL@GNL1004ZCBQC2>
2018-05-31 21:42:19 +0200

    Added area Welding

---
+++ EqUiWebUi/Areas/Welding/WeldingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.Welding
+{
+    public class WeldingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Welding";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Welding_default",
+                "Welding/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
