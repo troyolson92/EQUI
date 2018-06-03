f7e666c8ab291b86506b03d7e0e32bb6d246d908
SDEBEUL <SDEBEUL@volvocars.com>
2018-06-03 18:01:40 +0200

    added basic layout for welding area

---
+++ EqUiWebUi/Areas/Welding/Controllers/WeldingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.Welding
+{
+    public class WeldingController : Controller
+    {
+        // GET: Welding/Welding
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pagina1()
+        {
+            ViewBag.data1 = "Dit is een string";
+            ViewBag.data2 = 10;
+            return View();
+        }
+    }
+}
