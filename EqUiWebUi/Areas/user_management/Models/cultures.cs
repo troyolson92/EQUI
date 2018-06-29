a3374e6a35b4719c2074c8bf0d2748c5a9f2c19f
SDEBEUL <SDEBEUL@volvocars.com>
2018-06-29 14:03:35 +0200

    adding multilang support

---
+++ EqUiWebUi/Areas/user_management/Models/cultures.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.user_management.Models
+{
+    public static class cultures
+    {
+
+       public static SelectList CultureList()
+        {
+           return new SelectList(
+            new List<SelectListItem>
+            {
+                                new SelectListItem { Selected = true, Text = "English (en-GB)", Value = "en-GB"}, //default
+                                new SelectListItem { Selected = false, Text = "Dutch (nl-be)", Value = "nl-be"},
+                                new SelectListItem { Selected = false, Text = "Swedish (??)", Value = "??"},
+            }, "Value", "Text", 1);
+        }
+    }
+}
