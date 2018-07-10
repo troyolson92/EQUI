022e95533afe4e3d8b08da53050b2bdae690cf1e
SDEBEUL <SDEBEUL@volvocars.com>
2018-07-10 13:51:40 +0200

    changed for digibord

--- EqUiDigibordLauncher/digiboardLauncher.cs
+++ EqUiDigibordLauncher/digiboardLauncher.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using EQUICommunictionLib;
+﻿using EQUICommunictionLib;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EqUiDigibordLauncher
 {
-    public partial class Program
+    public partial class digiboardLauncher
     {
-        static void Main(string[] args)
+        public void RunLauncher()
         {
+
             Console.WriteLine("Hi I will start the screens for you");
             //
             ChromeTools chromeTools = new ChromeTools();
             //
-            Console.WriteLine("Getting configuration for: "+ Environment.UserDomainName+"\\"+Environment.UserName);
+            Console.WriteLine("Getting configuration for: " + Environment.UserDomainName + "\\" + Environment.UserName);
             //
             EQUICommunictionLib.ConnectionManager connectionManager = new ConnectionManager();
             string QRY = @"
@@ -25,13 +30,13 @@ order by screen_num
 
             DataTable dt = connectionManager.RunQuery(string.Format(QRY, Environment.UserDomainName + "\\" + Environment.UserName));
 
-            if (dt.Rows.Count != 0 )
+            if (dt.Rows.Count != 0)
             {
                 foreach (DataRow row in dt.Rows)
                 {
                     try
                     {
-                        Console.WriteLine("Launching to screenum: " + row.Field<int>("Screen_num")+ " ScreenID: " + row.Field<int>("id"));
+                        Console.WriteLine("Launching to screenum: " + row.Field<int>("Screen_num") + " ScreenID: " + row.Field<int>("id"));
                         string url = string.Format("http://equi/user_management/UserScreens/RenderUserScreen?screenID={0}", row.Field<int>("id"));
                         Console.WriteLine("url: " + url);
                         chromeTools.LaunchChrome(url, row.Field<int>("Screen_num"));
@@ -55,11 +60,11 @@ order by screen_num
             //Screenblock
             Console.WriteLine("Starting sleepblocker");
             PreventSleep();
-            while (true){
-                    Console.WriteLine("Leave me open I'll block the pc from faling asleep.");
-                    Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Leave me open I'll block the pc from faling asleep.");
+                Console.ReadLine();
             }
-
         }
 
         //helpers for sleepblock
