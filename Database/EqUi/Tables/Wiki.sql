6f16bb80abda566876313ee931222efdda6a2407
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-09 18:40:32 +0200

    Added database project

---
+++ Database/EqUi/Tables/Wiki.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [EqUi].[Wiki] (
+    [id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Title]       VARCHAR (50)  NULL,
+    [Description] VARCHAR (50)  NULL,
+    [Culture]     VARCHAR (50)  NULL,
+    [wiki]        VARCHAR (MAX) NULL,
+    [searchtags]  VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Wiki] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Database/Security/RoleMemberships.sql
@@ -0,0 +1,18 @@
+﻿ALTER ROLE [db_owner] ADD MEMBER [Hangfire];
+
+
+GO
+ALTER ROLE [db_accessadmin] ADD MEMBER [Hangfire];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [Hangfire];
+
+
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [EqUi];
+
+
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [Hangfire];
+
