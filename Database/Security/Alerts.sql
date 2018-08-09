a7c2a40ea5efa9b0146008eb02db08691f485997
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-09 18:40:32 +0200

    Added database project

---
+++ Database/Security/Alerts.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [Alerts]
+    AUTHORIZATION [dbo];
+
---
+++ Database/Volvo/Tables/c_Classification.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Volvo].[c_Classification] (
+    [id]             INT           IDENTITY (1, 1) NOT NULL,
+    [Classification] VARCHAR (50)  NOT NULL,
+    [Discription]    VARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_C_Classification] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
