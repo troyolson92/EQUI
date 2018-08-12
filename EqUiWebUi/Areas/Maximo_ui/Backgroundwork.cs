cf85a491b35c3aaf220c51a75959675f99d2d63f
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-12 12:05:54 +0200

    making maximo area generic

--- EqUiWebUi/Areas/Maximo_ui/Backgroundwork.cs
+++ EqUiWebUi/Areas/Maximo_ui/Backgroundwork.cs
@@ -19,27 +19,27 @@ namespace EqUiWebUi.Areas.Maximo_ui
             if (EqUiWebUi.MyBooleanExtensions.IsAreaEnabled("Maximo_ui"))
             {
                 Backgroundwork backgroundwork = new Backgroundwork();
-                RecurringJob.AddOrUpdate("MX-WO=>GADATA", () => backgroundwork.PushDatafromMAXIMOtoGADATA(null), Cron.HourInterval(1));  //set job to run every hour
-                RecurringJob.AddOrUpdate("MX-ASSETS=>GADATA", () => backgroundwork.UpdateMaximoAssetsToGadata(), "0 12 * * 0");            //set job to run sunday at 12
+                RecurringJob.AddOrUpdate("MX-WO=>EQUI", () => backgroundwork.PushDatafromMAXIMOtoEQUI(null), Cron.HourInterval(1));  //set job to run every hour
+                RecurringJob.AddOrUpdate("MX-ASSETS=>EQUI", () => backgroundwork.UpdateMaximoAssetsToEQUI(null), "0 12 * * 0");            //set job to run sunday at 12
             }
             else
             {
                 log.Warn("Maximo_ui area is disabled removing hangfire jobs");
-                RecurringJob.RemoveIfExists("MX-WO=>GADATA");
-                RecurringJob.RemoveIfExists("MX-ASSETS=>GADATA");
+                RecurringJob.RemoveIfExists("MX-WO=>EQUI");
+                RecurringJob.RemoveIfExists("MX-ASSETS=>EQUI");
             }
         }
 
         //update new data from STO to gadata. called every minute #hangfire
         [AutomaticRetry(Attempts = 0)]
         [Queue("gadata")]
-        public void PushDatafromMAXIMOtoGADATA(PerformContext context)
+        public void PushDatafromMAXIMOtoEQUI(PerformContext context)
         {
             //delete data in now in maximo.
             ConnectionManager connectionManager = new ConnectionManager();
-            context.WriteLine(" Delete workorders in gadata started");
-            connectionManager.RunCommand("DELETE GADATA.MAXIMO.WORKORDERS FROM GADATA.MAXIMO.WORKORDERS");
-            context.WriteLine(" Done");
+            context.WriteLine("Delete workorders started");
+            connectionManager.RunCommand("DELETE [MAXIMO].[WORKORDERS] FROM [MAXIMO].[WORKORDERS]", enblExeptions:true);
+            context.WriteLine("Done");
 
             //get new records from STO
             string MaximoQry = string.Format(@"
@@ -57,43 +57,45 @@ from MAXIMO.WORKORDER WORKORDER
 join MAXIMO.locancestor locancestor on 
 locancestor.LOCATION = WORKORDER.LOCATION
 and 
-locancestor.ORGID = 'VCCBE'
+locancestor.ORGID = '{0}'
 and
-locancestor.ANCESTOR LIKE 'A'
+locancestor.ANCESTOR LIKE '{1}'
 WHERE
 workorder.status not in ('CLOSE','CLOSEVOID') --they changes something where they set a shitload of workorders in this state. (the changedate was updated)
 and
 workorder.changedate >= sysdate - 100
 "
-);
+,System.Configuration.ConfigurationManager.AppSettings["Maximo_ORGID"].ToString() //siteID
+,System.Configuration.ConfigurationManager.AppSettings["Maximo_TOPANCESTOR"].ToString()); //Top level ancestor location to get workorders from
 
-            context.WriteLine(" Get workorders on MAXIMOrt started");
             DataTable newMaximoDt;
             try
             {
                 //try realtime connection
+                context.WriteLine("Get workorders from maximo started (useing: MAXIMOrt)");
                 newMaximoDt = connectionManager.RunQuery(MaximoQry, dbName: "MAXIMOrt", maxEXECtime: 120, enblExeptions: true);
-                context.WriteLine(" Done");
+                context.WriteLine("Done");
             }
             catch (Exception ex)
             {
-                context.WriteLine(" FAILURE for MAXIMOrt connection! " + ex.Message);
-                log.Error(" FAILURE for MAXIMOrt connection", ex);
+                context.WriteLine("FAILURE for MAXIMOrt connection! " + ex.Message);
+                log.Error("FAILURE for MAXIMOrt connection", ex);
                 //if fails try reporting db
+                context.WriteLine("giving it another try using MAXIMO7rep");
                 newMaximoDt = connectionManager.RunQuery(MaximoQry, dbName: "MAXIMO7rep", maxEXECtime: 120, enblExeptions: true);
-                context.WriteLine(" Done (used MAXIMO7rep)");
+                context.WriteLine("Done (used MAXIMO7rep)");
             }
             //push to gadata
-            context.WriteLine(" Push workorders to gadata started");
-            connectionManager.BulkCopy(newMaximoDt, "[MAXIMO].[WORKORDERS]");
-            context.WriteLine(" Done");
+            context.WriteLine(string.Format("BulkCopy started (rowcount:{0})", newMaximoDt.Rows.Count));
+            connectionManager.BulkCopy(newMaximoDt, "[MAXIMO].[WORKORDERS]",enblExeptions:true);
+            context.WriteLine("Done");
         }
 
         //update asset table on GADATA from maximo called every sunday #hangfire
         [Queue("gadata")]
         [AutomaticRetry(Attempts = 5)]
         //This is for sync Mx7 with the database. 
-        public void UpdateMaximoAssetsToGadata()
+        public void UpdateMaximoAssetsToEQUI(PerformContext context)
         {
             //get asset list from maximo M7 daily copy
             string strSqlGetFromMaximo = string.Format(@"
@@ -162,30 +164,30 @@ workorder.changedate >= sysdate - 100
 
             DataTable tableFromMx7 = new DataTable();
             ConnectionManager connectionManager = new ConnectionManager();
-            log.Info("DataFromMaximo start");
+            context.WriteLine("GetdataFromMaximo start");
             tableFromMx7 = connectionManager.RunQuery(strSqlGetFromMaximo, dbName: "MAXIMO7rep", enblExeptions: true, maxEXECtime: 300);
-            log.Info("DataFromMaximo Rowcount: " + tableFromMx7.Rows.Count);
+            context.WriteLine("GetdataFromMaximo Rowcount: " + tableFromMx7.Rows.Count);
             //clear destination table  in GADATA
             string CmdDeleteTableData = @"DELETE FROM [Equi].[ASSETS_fromMX7] FROM [Equi].[ASSETS_fromMX7]";
-            log.Info("GadataDelete Start");
+            context.WriteLine("DataDelete Start");
             connectionManager.RunCommand(CmdDeleteTableData, enblExeptions: true, maxEXECtime: 300);
-            log.Info("GadataDelete Done");
+            context.WriteLine("DataDelete Done");
             //send the data to the SQL GADATA SERVER 
-            log.Info("BulkCopyToGadata Start");
+            context.WriteLine("BulkCopy Start");
             connectionManager.BulkCopy(tableFromMx7, "[EQUI].[ASSETS_fromMX7]", enblExeptions: true, maxEXECtime: 300);
-            log.Info("BulkCopyToGadata End");
-            //Run command to link the assets and update c_controllerTables
-            LinkMaximoAssetsToGadata();
+            context.WriteLine("BulkCopy Done");
+            //Run command to link the assets and update c_controller tables
+            LinkMaximoAssetsToGadata(context);
         }
 
         //This relinks the assets from maximo in gadata 
-        public void LinkMaximoAssetsToGadata()
+        public void LinkMaximoAssetsToGadata(PerformContext context = null)
         {
-            log.Info("sp_linkassets Start");
+            context.WriteLine("sp_linkassets Start");
             string CmdLinkAssets = "EXEC [EqUi].[sp_LinkAssets]";
             ConnectionManager connectionManager = new ConnectionManager();
             connectionManager.RunCommand(CmdLinkAssets, enblExeptions: true, maxEXECtime: 300);
-            log.Info("sp_linkassets Done");
+            context.WriteLine("sp_linkassets Done");
         }
     }
 }
