900d863e9d3ea1c07fcfd1dd279dd6a577716e45
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-13 13:11:20 +0200

    ADDED VCSC area and option to disbale it

---
+++ EqUiWebUi/Areas/VCSC/VCSCAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.VCSC
+{
+    public class VCSCAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "VCSC";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            if (EqUiWebUi.MyBooleanExtensions.IsAreaEnabled(AreaName))
+            {
+                context.MapRoute(
+                "VCSC_default",
+                "VCSC/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+            }
+        }
+    }
+}
