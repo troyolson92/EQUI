dd4641f47a127682f8f5aa035e63ab3b7ec9f90a
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-19 10:36:09 +0200

    added VWSC diagrams

---
+++ EqUiWebUi/Areas/VWSC/VWSCAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.VWSC
+{
+    public class VWSCAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "VWSC";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "VWSC_default",
+                "VWSC/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
