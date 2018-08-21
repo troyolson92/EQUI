82f7a18090dead56403145569e5210235ddc7e6c
SDEBEUL <SDEBEUL@volvocars.com>
2018-08-21 14:21:28 +0200

    sync for TADATA

---
+++ Database/Security/UserAndRoles.sql
@@ -0,0 +1,26 @@
+﻿CREATE LOGIN [VASC_a] WITH PASSWORD=N'JweDpPYdey5/9ke2D/IYUuTzR2KqfUB4I23iL+2GV1o=', DEFAULT_DATABASE=[$(DatabaseName)], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+CREATE USER [VASC_a] FOR LOGIN [VASC_a];
+GO
+CREATE LOGIN [EqUiAdmin] WITH PASSWORD=N'akpC7iTebRcNCl/n/xVVP7g9IMn99R5idbGD11isOSk=', DEFAULT_DATABASE=[$(DatabaseName)], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+CREATE USER [EqUiAdmin] FOR LOGIN [EqUiAdmin];
+GO
+CREATE ROLE [db_accessEquiAdmin] AUTHORIZATION [dbo];
+
+GO
+GRANT SELECT TO [db_accessEquiAdmin];
+GO
+GRANT INSERT TO [db_accessEquiAdmin];
+GO
+GRANT UPDATE TO [db_accessEquiAdmin];
+GO
+GRANT ALTER TO [db_accessEquiAdmin];
+GO
+GRANT DELETE TO [db_accessEquiAdmin];
+GO
+GRANT EXECUTE TO [db_accessEquiAdmin];
+GO
+ALTER ROLE [db_accessEquiAdmin] ADD MEMBER [EqUiAdmin];
+GO
+ALTER ROLE [db_accessEquiAdmin] ADD MEMBER [VASC_a];
