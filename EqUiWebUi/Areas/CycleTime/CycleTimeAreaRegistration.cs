2df96dc4aed6d5feabe0fcd183a88f9ee628f255
SDEBEUL <SDEBEUL@volvocars.com>
2018-10-03 12:23:36 +0200

    adding cycletime area

---
+++ EqUiWebUi/Areas/CycleTime/CycleTimeAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace EqUiWebUi.Areas.CycleTime
+{
+    public class CycleTimeAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CycleTime";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            if (EqUiWebUi.MyBooleanExtensions.IsAreaEnabled(AreaName))
+            {
+                context.MapRoute(
+                "CycleTime_default",
+                "CycleTime/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+                );
+            }
+        }
+    }
+}
