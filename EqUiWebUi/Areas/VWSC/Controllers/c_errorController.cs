0fadefcad303f80073b2c4630dbcf608232cd18f
SDEBEUL <SDEBEUL@volvocars.com>
2018-10-04 16:12:52 +0200

    end of day. (more work on vwsc UI)

--- EqUiWebUi/Areas/VWSC/Controllers/c_errorController.cs
+++ EqUiWebUi/Areas/VWSC/Controllers/c_errorController.cs
@@ -1,63 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using EqUiWebUi.Areas.VWSC.Models;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
-using EqUiWebUi.Areas.VWSC.Models;
 using static EqUiWebUi.Areas.VWSC.Models.VWSCenums;
 
 namespace EqUiWebUi.Areas.VWSC.Controllers
 {
     [Authorize(Roles = "Administrator")]
-    public class c_bosch_viewController : Controller
+    public class c_errorController : Controller
     {
+
         private GADATAEntitiesVWSC db = new GADATAEntitiesVWSC();
 
-        // GET: VWSC/c_bosch_view
+        // GET:VWSC/c_error
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: VWSC/c_bosch_view/_List
+        // GET: VWSC/c_error/_List
         //Will return partial view with a list of the controllers in a controller class.
         //Filterable by enable bit
         public ActionResult _List(int? enable_mask, int? timer_id)
         {
             ViewBag.timer_id = timer_id;
-            List<VWSC_c_bosch_view> list = new List<VWSC_c_bosch_view>();
+            List<VWSC_c_error> list = new List<VWSC_c_error>();
             if (enable_mask is null)
             {
-                list = db.c_bosch_view.ToList();
+                list = db.c_error.ToList();
             }
             else
             {
                 var setbits = Enumerable.Range(0, 32).Where(x => ((enable_mask >> x) & 1) == 1);
                 foreach (int setbit in setbits)
                 {
-                    list.AddRange(db.c_bosch_view.Where(c => c.enable_bit == setbit + 1 && c.enable_bit != 0).ToList());
+                    list.AddRange(db.c_error.Where(c => c.enable_bit == setbit + 1 && c.enable_bit != 0).ToList());
                 }
             }
             return PartialView(list);
         }
 
-        // GET: VWSC/c_bosch_view/Details/5
+        // GET: VWSC/c_error/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            VWSC_c_bosch_view c_Bosch_View = db.c_bosch_view.Find(id);
-            if (c_Bosch_View == null)
+            VWSC_c_error c_error = db.c_error.Find(id);
+            if (c_error == null)
             {
                 return HttpNotFound();
             }
-            return View(c_Bosch_View);
+            return View(c_error);
         }
 
 
-        // GET: VWSC/c_bosch_view/Edit/5
+        // GET: VWSC/c_error/Edit/5
         // We will handle the creation of a new trigger also in EDIT. (to make code simplere) to create a new trigger pass ID = -1
         public ActionResult Edit(int? id)
         {
@@ -65,71 +68,71 @@ namespace EqUiWebUi.Areas.VWSC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            VWSC_c_bosch_view c_bosch_view;
+            VWSC_c_error c_error;
 
             if (id == -1) //create new
             {
-                c_bosch_view = new VWSC_c_bosch_view();
-                c_bosch_view.enable_bit = (int)Enable_bit.Disabled;
+                c_error = new VWSC_c_error();
+                c_error.enable_bit = (int)Enable_bit.Disabled;
             }
             else //find the existing alert 
             {
-                c_bosch_view = db.c_bosch_view.Find(id);
+                c_error = db.c_error.Find(id);
 
-                if (c_bosch_view == null)
+                if (c_error == null)
                 {
                     return HttpNotFound();
                 }
             }
-            return View(c_bosch_view);
+            return View(c_error);
         }
 
-        // POST: VWSC/c_bosch_view/Edit/5
+        // POST: VWSC/c_error/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(VWSC_c_bosch_view c_bosch_view)
+        public ActionResult Edit(VWSC_c_error c_error)
         {
             if (ModelState.IsValid)
             {
-                if (c_bosch_view.id == -1)//add new 
+                if (c_error.id == -1)//add new 
                 {
-                    db.c_bosch_view.Add(c_bosch_view);
+                    db.c_error.Add(c_error);
                 }
                 else
                 {
-                    db.Entry(c_bosch_view).State = EntityState.Modified;
+                    db.Entry(c_error).State = EntityState.Modified;
                 }
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(c_bosch_view);
+            return View(c_error);
         }
 
-        // GET: VWSC/c_bosch_view/Delete/5
+        // GET: VWSC/c_error/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            VWSC_c_bosch_view c_bosch_view = db.c_bosch_view.Find(id);
-            if (c_bosch_view == null)
+            VWSC_c_error c_error = db.c_error.Find(id);
+            if (c_error == null)
             {
                 return HttpNotFound();
             }
-            return View(c_bosch_view);
+            return View(c_error);
         }
 
-        // POST: VWSC/c_bosch_view/Delete/5
+        // POST: VWSC/c_error/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            VWSC_c_bosch_view c_bosch_view = db.c_bosch_view.Find(id);
-            db.c_bosch_view.Remove(c_bosch_view);
+            VWSC_c_error c_error = db.c_error.Find(id);
+            db.c_error.Remove(c_error);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
---
+++ EqUiWebUi/Areas/VWSC/Models/VWSC_c_errorMetadata.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using static EqUiWebUi.Areas.VWSC.Models.VWSCenums;
+
+namespace EqUiWebUi.Areas.VWSC.Models
+{
+    public class VWSC_c_errorMetadata
+    {
+    }
+
+    [MetadataType(typeof(VWSC_c_error))]
+    public partial class VWSC_c_error
+    {
+
+        public Enable_bit _Enable_bit
+        {
+            get
+            {
+                return (Enable_bit)Enum.ToObject(typeof(Enable_bit), this.enable_bit);
+            }
+            set
+            {
+                this.enable_bit = (int)value;
+            }
+        }
+
+        public LogicOperator _C_operator
+        {
+            get
+            {
+                return (LogicOperator)Enum.ToObject(typeof(LogicOperator), this.C_operator);
+            }
+            set
+            {
+                this.C_operator = (int)value;
+            }
+        }
+
+        public c_error_flags _Flags
+        {
+            get
+            {
+                return (c_error_flags)Enum.ToObject(typeof(c_error_flags), this.flags);
+            }
+            set
+            {
+                this.flags = (int)value;
+            }
+        }
+
+
+    }
+}
