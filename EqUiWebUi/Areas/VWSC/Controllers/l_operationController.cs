10e513e55a62d4160bab9cdd90dd5bb45662c655
SDEBEUL <SDEBEUL@volvocars.com>
2018-10-05 09:03:56 +0200

    sync before first deploy

---
+++ EqUiWebUi/Areas/VWSC/Controllers/l_operationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EqUiWebUi.Areas.VWSC.Models;
+
+namespace EqUiWebUi.Areas.VWSC.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class L_operationController : Controller
+    {
+        private GADATAEntitiesVWSC db = new GADATAEntitiesVWSC();
+
+        // GET: VWSC/L_operation
+        public ActionResult Index(int? timer_id, string sessionName)
+        {
+            ViewBag.timer_id = timer_id;
+            ViewBag.sessionName = sessionName;
+            return View();
+        }
+
+        // GET: VWSC/L_operation/_List
+        //Will return partial view with a list of the L_operation.
+        public ActionResult _List(int? timer_id, string sessionName)
+        {
+            ViewBag.timer_id = timer_id;
+            if (timer_id is null)
+            {
+                if (sessionName is null)
+                {
+                    //all 
+                    return PartialView(db.L_operation);
+                }
+                else
+                {
+                    //for a session
+                    return PartialView(db.L_operation.Where(c => c.Vwsc_name.Contains(sessionName)));
+                }
+            }
+            else
+            {
+                //for a controller
+                return PartialView(db.L_operation.Where(c => (c.controller_id == timer_id && c.Vwsc_name.Contains(sessionName)) || (c.controller_id == null && c.Vwsc_name.Contains(sessionName))));
+            }
+
+        }
+    }
+}
---
+++ EqUiWebUi/Areas/VWSC/Controllers/rt_active_infoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EqUiWebUi.Areas.VWSC.Models;
+using static EqUiWebUi.Areas.VWSC.Models.VWSCenums;
+
+namespace EqUiWebUi.Areas.VWSC.Controllers
+{
+    public class rt_active_infoController : Controller
+    {
+        private GADATAEntitiesVWSC db = new GADATAEntitiesVWSC();
+
+        // GET: VWSC/rt_active_info
+        public ActionResult Index(bool ShowNOKonly = false)
+        {
+            if (ShowNOKonly)
+            {
+                return View(db.rt_active_info.Where(c => c.vwsc_state != (int)VWSCState.STATE_CONNECTED).Include(r => r.Timer));
+            }
+            else
+            {
+                return View(db.rt_active_info.Include(c => c.Timer));
+            }
+        }
+
+        // GET: VWSC/rt_active_info/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VWSC_rt_active_info rt_active_info = db.rt_active_info.Find(id);
+            if (rt_active_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rt_active_info);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
