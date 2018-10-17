7c2c427514635fe09557a027945a04f9a686e83b
SDEBEUL <sdebeul@volvocars.com>
2018-10-17 11:53:11 +0200

    basic vwsc grids up

--- EqUiWebUi/Areas/VWSC/Controllers/vwscController.cs
+++ EqUiWebUi/Areas/VWSC/Controllers/vwscController.cs
@@ -25,7 +25,19 @@ namespace EqUiWebUi.Areas.VWSC.Controllers
             return View(data);
         }
 
-        public ActionResult _welds(int timerId, int weldmeasureprotddw_id_Start, int weldmeasureprotddw_id_End)
+        public ActionResult _jobdetails(int id)
+        {
+            VWSC_rt_job job = db.rt_job.Find(id);
+            return PartialView(job);
+        }
+
+        public ActionResult _jobBreakdown(int id)
+        {
+            IQueryable<VWSC_rt_job_breakdown> breakdowns = db.rt_job_breakdown.Where(c => c.rt_job_id == id).AsQueryable();
+            return PartialView(breakdowns);
+        }
+
+        public ActionResult _weldMeasure(int timerId, int weldmeasureprotddw_id_Start, int weldmeasureprotddw_id_End)
         {
             IQueryable<VWSC_rt_weldmeasureprotddw> data = db.rt_weldmeasureprotddw.Where(c => 
             c.timerId == timerId 
@@ -34,5 +46,17 @@ namespace EqUiWebUi.Areas.VWSC.Controllers
             ).AsQueryable();
             return PartialView(data);
         }
+
+        public ActionResult _weldFault(int timerId, int weldfaultprot_id_Start, int weldfaultprot_id_End)
+        {
+            IQueryable<VWSC_rt_weldfault> data = db.rt_weldfault.Where(c =>
+            c.timerId == timerId
+            && c.id >= weldfaultprot_id_Start
+            && c.id <= weldfaultprot_id_End
+            ).AsQueryable();
+            return PartialView(data);
+        }
+
+
     }
 }
