6460d8a5d3f993bb4b833dbed65e696f149e2cfb
SDEBEUL <sdebeul@volvocars.com>
2018-10-19 12:54:19 +0200

    sync

--- EqUiWebUi/Areas/Welding/Models/WeldingModel.Designer.cs
+++ EqUiWebUi/Areas/Welding/Models/WeldingModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\jcoppej1\Documents\GitHub\ExcelPluginFrontEnd\EqUiWebUi\Areas\Welding\Models\WeldingModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\SDEBEUL\source\repos\ExcelPluginFrontEnd\EqUiWebUi\Areas\Welding\Models\WeldingModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
