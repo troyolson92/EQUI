239daa8f14863dfa020dc4dec3f0446fc1402332
SDEBEUL <sdebeul@volvocars.com>
2018-12-12 15:39:32 +0100

    updates cycletime api but it does not work.

--- EqUiWebUi/Areas/CycleTime/BackgroundWork.cs
+++ EqUiWebUi/Areas/CycleTime/BackgroundWork.cs
@@ -3,6 +3,7 @@ using Hangfire.Console;
 using Hangfire.Server;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace EqUiWebUi.Areas.CycleTime
@@ -60,26 +61,56 @@ namespace EqUiWebUi.Areas.CycleTime
             System.DateTime fromDate = System.DateTime.Now.AddDays(-11);
             System.DateTime toDate = System.DateTime.Now.AddDays(-10);
             const string Format = "yyyyMMddHHmmss0000";
-            string model = carmodels.XC40;
+            int bodyType = 151110; //random body type
             int ignoreBelow = 20;
-            context.WriteLine($"Get GetP4CycleTime from:{fromDate} to:{toDate} model:{model} ignoreBelow:{ignoreBelow}");
+            context.WriteLine($"Get GetP4CycleTime from:{fromDate} to:{toDate} bodyType:{bodyType} ignoreBelow:{ignoreBelow}");
             //
             var client = new RestClient("http://gensvw2107.gen.volvocars.net/CMACTAPP/api/");
-            var request = new RestRequest("GetCyclesP4/GetOverView?fromDate={fromDate}&toDate={toDate}&model={model}&ignoreBelow={ignoreBelow}", Method.GET);
+            var request = new RestRequest("GetCyclesP4/GetOverView?fromDate={fromDate}&toDate={toDate}&bodyType={bodyType}&ignoreBelow={ignoreBelow}", Method.GET);
             request.AddParameter("fromDate", fromDate.ToString(Format), ParameterType.UrlSegment);
             request.AddParameter("toDate", toDate.ToString(Format), ParameterType.UrlSegment);
-            request.AddParameter("model", model, ParameterType.UrlSegment);
+            request.AddParameter("bodyType", bodyType.ToString(), ParameterType.UrlSegment);
             request.AddParameter("ignoreBelow", ignoreBelow.ToString(), ParameterType.UrlSegment);
-
             request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
-     
             var queryResult = client.Execute(request);
-
+            //
             context.WriteLine(queryResult.Content);
-
-            context.WriteLine("end");
-            //handle data
-
         }
     }
+
+    public class OverviewKPIP4
+    {
+        public string Linenumber { get; set; }
+        public string Linename { get; set; }
+        public List<OverViewStationP4> Stations { get; set; }
+        public OverviewKPIP4()
+        {
+            Stations = new List<OverViewStationP4>();
+        }
+    }
+    public class OverViewStationP4
+    {
+        public string IMZONE { get; set; } //Mode zone
+        public string STATION { get; set; } //Station
+        public string Area { get; set; } // Area name given
+        public string Line { get; set; }
+        public double LCT { get; set; }
+
+        public double PCTL_05 { get; set; } //not used external
+        public double PCTL_05_sec { get; set; } //T05 seconds
+        public double PCTL_50 { get; set; } //not used external
+        public double PCTL_50_sec { get; set; } //T50 seconds
+        public double PCTL_90 { get; set; } //not used external
+        public double PCTL_90_sec { get; set; } //T90 seconds
+
+        public double PCTL_95 { get; set; }//not used external
+        public double PCTL_95_sec { get; set; } //T95 seconds
+
+        public double Weeklyaverage { get; set; } //Not used external
+        public double Weeklyaverage_sec { get; set; } //Weekly avg seconds
+        public int WeeklyCTCount { get; set; } // counts bodies, initially was for weekly intended, but should work for given period as well.
+
+    }
+
+
 }
