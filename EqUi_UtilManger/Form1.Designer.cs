54c2c7a2f979afb5866d5b0581e9e5da7bd3ac6d
SDEBEUL <sdebeul@volvocars.com>
2018-12-22 11:15:06 +0100

    removed "debugger" from coms lib

--- EqUi_UtilManger/Form1.Designer.cs
+++ EqUi_UtilManger/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.btn_sbcuStat = new System.Windows.Forms.Button();
-            this.btn_blockSleep = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_sbcuStat
@@ -43,37 +42,22 @@
             this.btn_sbcuStat.UseVisualStyleBackColor = true;
             this.btn_sbcuStat.Click += new System.EventHandler(this.btn_sbcuStat_Click);
             // 
-            // btn_blockSleep
-            // 
-            this.btn_blockSleep.AutoSize = true;
-            this.btn_blockSleep.Location = new System.Drawing.Point(20, 47);
-            this.btn_blockSleep.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_blockSleep.Name = "btn_blockSleep";
-            this.btn_blockSleep.Size = new System.Drawing.Size(100, 23);
-            this.btn_blockSleep.TabIndex = 6;
-            this.btn_blockSleep.Text = "BlockSleep";
-            this.btn_blockSleep.UseVisualStyleBackColor = true;
-            this.btn_blockSleep.Click += new System.EventHandler(this.btn_blockSleep_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(131, 85);
-            this.Controls.Add(this.btn_blockSleep);
+            this.ClientSize = new System.Drawing.Size(131, 40);
             this.Controls.Add(this.btn_sbcuStat);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_sbcuStat;
-        private System.Windows.Forms.Button btn_blockSleep;
     }
 }
 
