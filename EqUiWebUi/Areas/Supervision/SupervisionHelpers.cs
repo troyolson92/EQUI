e6de48878f459ec16b9aca5ad746ae74d53fe5b6
troyolson92 <47667946+troyolson92@users.noreply.github.com>
2019-02-15 10:05:39 -0500

    20190215 TDO test color change
    20190215 TDO (troy's intial's) test color change VCCH

--- EqUiWebUi/Areas/Supervision/SupervisionHelpers.cs
+++ EqUiWebUi/Areas/Supervision/SupervisionHelpers.cs
@@ -12,14 +12,17 @@
                     return "TableLogtypeRed"; //marks a breakdown that is ongoing 
 
                 case "BREAKDOWN":
+                    return "TableLogtypeDarkYellow"; //marks a breakdown that has been resolved //was blank 20190215 TDO
                 case "STOerror":
-                    return "TableLogtypeDarkYellow"; //marks a breakdown that has been resolved
+                    return ".TableStatusGrey"; //marks a breakdown that has been resolved
+//                     //return "TableLogtypeDarkYellow"; //marks a breakdown that has been resolved 20190215 TDO
 
                 case "TIMELINE":
                     return "TableLogtypeLightGreen"; //marks begin of each new production shift
 
                 case "ALERT": //COMPLETED ALERT! alert that has been resolved 
-                    return "TableLogtypeLightBlue";
+                   // return "TableLogtypeLightBlue"; 20190215 TDO
+                    return .TableStatusLightGrey;
 
                 default:
                     return logtype; //this is used for alerts the pass the animation directly (when alert is active else they just pass "ALERT"
